f9863b73fdc895bf370bb96dd5151270e2234c15
Kagen Hearn <kl.hearn40@gmail.com>
2021-07-01 01:50:51 -0400

    Fixed bug with card event listener

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const cards = document.querySelectorAll(".overall-box");
     cards.forEach(card=>{
         card.addEventListener("click", e=>{
-            window.location.href = `/stories/${e.target.id.split("-")[2]}`;
+            // console.log(e.target.id.split("-")[2]);
+            window.location.href = `/stories/${card.id.split("-")[2]}`;
         })
     })
 })
